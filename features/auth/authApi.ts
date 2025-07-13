c1a85bf8c18a05829b04b70c8701e6db8381bdc6
sfaa <ym6772003@gmail.com>
2025-07-13 11:44:34 -0700

    axios

--- features/auth/authApi.ts
+++ features/auth/authApi.ts
@@ -1,5 +1,6 @@
 import { apiClient } from "@/lib/api";
 import { User } from "../../types/user";
+import axios from "axios";
 
 
 export interface LoginPayload {
@@ -32,7 +33,9 @@ export interface picture{
 }
 
 export const login = async (data: LoginPayload): Promise<userReturnRequest> => {
-   const response = await apiClient.post('/api/login', data, { withCredentials: true });
+  //  const response = await apiClient.post('/auth/login', data, { withCredentials: true });
+   const response = await axios.post('/api/login', data, { withCredentials: true });
+
   return response.data;
 };
 
