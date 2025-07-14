4411e3146c2de07f5cab3c6d201146e800afe672
sfaa <ym6772003@gmail.com>
2025-07-13 18:00:29 -0700

    add api

--- features/user/userApi.ts
+++ features/user/userApi.ts
@@ -2,17 +2,17 @@ import { apiClient } from "@/lib/api";
 import axios from "axios";
 
 export const fetchUsers = async (page: number, limit: number) => {
-  const response = await axios.get(`/users?page=${page}&limit=${limit}`,{withCredentials:true});
+  const response = await axios.get(`/api/users?page=${page}&limit=${limit}`,{withCredentials:true});
   return response.data;
 }
 
 export const deleteUser = async (userId: string) => {
-  const response = await axios.delete(`/users/${userId}`, { withCredentials: true });
+  const response = await axios.delete(`/api/users/${userId}`, { withCredentials: true });
   return response.data;
 }
 
 export const approveUser = async (userId: string) => {
-  const response = await axios.put(`/admin/approve-provider/${userId}`,  { withCredentials: true });
+  const response = await axios.put(`/api/admin/approve-provider/${userId}`,  { withCredentials: true });
   return response.data;
 }
 
@@ -35,7 +35,7 @@ export const getUserById = async (userId: string) => {
 }
 
 export const updateUser = async (userId: string, userData: any) => {
-  const response = await axios.put(`/users/${userId}`, userData, { withCredentials: true });
+  const response = await axios.put(`/api/users/${userId}`, userData, { withCredentials: true });
   return response.data;
 }
 
